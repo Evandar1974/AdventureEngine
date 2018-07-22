59f38b1ee0432281f432897314b7690dfd3325d4
Mark <markjcarroll@hotmail.co.uk>
2018-07-22 13:50:19 +0100

    Added Level manager and splash screen

---
+++ Adventure Engine/Assets/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿//Scene manager with simple methods to load scenes and quit game
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public float autoLoadafter;
+
+    private void Start()
+    {
+        if (autoLoadafter > 0)
+        {
+            Invoke("LoadNextLevel", autoLoadafter);
+        }
+
+    }
+    public void LoadLevel(string name)
+    {
+
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceenIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceenIndex + 1);
+    }
+}
---
+++ Adventure Engine/Assets/Scripts/Utils.cs
@@ -0,0 +1,26 @@
+﻿// raycast layers
+public enum Layer
+{
+    RaycastEndStop = -1,
+    Enemy = 9,
+    Walkable = 10,
+    Usable = 11,
+    Pickup = 12
+}
+
+//Game manager states
+public enum GameStates
+{
+    MainMenu = 0,
+    Game = 1,
+    Notes = 2,
+    Inventory = 3,
+    Pause = 4,
+    ReadNote = 5,
+    Container = 6,
+    ActionsList = 7,
+    Death = 8,
+    Loading = 9,
+    GameComplete = 10,
+    PasswordInput = 11
+}
