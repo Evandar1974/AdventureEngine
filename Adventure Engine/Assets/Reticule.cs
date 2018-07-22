a6853999ea10150390c717f70a4b7eeb5f623195
Mark <markjcarroll@hotmail.co.uk>
2018-07-22 11:31:41 +0100

    Added reticule to UI atched script to reticule to use raycaster atached to first person camera, created Enum in Utils script for the raycast layers

---
+++ Adventure Engine/Assets/Reticule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticule : MonoBehaviour
+{
+    CameraRaycaster cameraRaycaster;
+    // Use this for initialization
+    void Start()
+    {
+
+        cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(cameraRaycaster.layerHit);
+    }
+
+}
+
---
+++ Adventure Engine/Assets/Utils.cs
@@ -0,0 +1,8 @@
+﻿public enum Layer
+{
+    RaycastEndStop = -1,
+    Enemy = 9,
+    Walkable = 10,
+    Usable = 11,
+    Pickup = 12
+}
